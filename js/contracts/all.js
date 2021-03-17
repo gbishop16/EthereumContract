7ae9477e55765f87dcb6d30eb7c76b5c7771fdac
gbishop16 <gqbishop@gmail.com>
2021-03-17 09:00:53 -0700

    stuff

--- js/contracts/all.js
+++ js/contracts/all.js
@@ -1,8 +1,9 @@
 // Grab the contract application binaray interface from
 // http://remix.ethereum.org/#optimize=false&runs=200&evmVersion=null&version=soljson-v0.7.4+commit.3f05b770.js
 // Go to Solidity Compiler -> Compile -> Compilation Details -> Contract ABI
-var AdvTopicsAddress = "0x71DeDeA117Fa2a2E47F81186b3500cD9796613Ff";
-var AdvTopicsContractABI = [
+//0xd63363d4A64A40662D5c2572130B586fa8D5dF4F
+var AdvTopicsAddress = "0x9E1154bBE465e38b93EE38A656B5dC0e7bc76634";
+var AdvTopicsContractABI =[
 	{
 		"inputs": [
 			{
